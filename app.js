159467a2e21c0f3f58fa7662a94b742a8d18377d
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-29 16:12:21 +0200

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById('search-job').addEventListener('click', function() {
+    alert('Tính năng tìm kiếm việc đang được phát triển!');
+});
+
+document.getElementById('manage-profile').addEventListener('click', function() {
+    alert('Tính năng quản lý hồ sơ đang được phát triển!');
+});
+
+document.getElementById('connect-employers').addEventListener('click', function() {
+    alert('Tính năng kết nối với nhà tuyển dụng đang được phát triển!');
+});
+
+document.getElementById('discussion-forum').addEventListener('click', function() {
+    alert('Tính năng diễn đàn thảo luận đang được phát triển!');
+});
+
+document.getElementById('resources').addEventListener('click', function() {
+    alert('Tính năng tài nguyên học tập đang được phát triển!');
+});
